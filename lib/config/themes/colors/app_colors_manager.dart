3ad973d87024465634901cb34a0a35095ee62d00
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-06-19 23:04:12 +0300

    - add most shared colors

---
+++ lib/config/themes/colors/app_colors_manager.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColorsManager {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color primaryActive = Color(0xFFF25700);
+  static const Color primaryInactive = Color(0xFFEAECF0);
+  static const Color primaryBackground = Color(0xFFFBFBFB);
+  static const Color primaryText = Color(0xFF1E1E1E);
+  static const Color secondaryText = Color(0xFF959595);
+}
